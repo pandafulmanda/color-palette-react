6f11bf6073dbffd0587fd032e7acaeee65142189
amanda <pandafulmanda@gmail.com>
2019-11-23 12:54:45 -0600

    Updates

---
+++ precache-manifest.c979038af64446ae9d917bdc43b1447a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9745866671d48f2c9a86af4cdc6044a",
+    "url": "/color-palette-react/index.html"
+  },
+  {
+    "revision": "a0b892240dce7d811b10",
+    "url": "/color-palette-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "262f1823282de26e4f22",
+    "url": "/color-palette-react/static/js/2.a19e4495.chunk.js"
+  },
+  {
+    "revision": "a0b892240dce7d811b10",
+    "url": "/color-palette-react/static/js/main.0cf1a53c.chunk.js"
+  },
+  {
+    "revision": "9b189783689f3d4b4dcc",
+    "url": "/color-palette-react/static/js/runtime-main.e0845248.js"
+  }
+]);
